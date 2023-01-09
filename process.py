b8f9e1a786d4e8c3b40170a2ad44d21ef9df0c3e
ohmzth <118098199+ohmzth@users.noreply.github.com>
2023-01-09 17:05:48 +0700

    add model

--- process.py
+++ process.py
@@ -11,6 +11,54 @@ import sys
 from time import time
 sys.setrecursionlimit(10 ** 9)
 
+class detection:
+    def __init__(self,capture_index,model_name):
+        """
+        Initializes the class with youtube url and output file.
+        :param url: Has to be as youtube URL,on which prediction is made.
+        :param out_file: A valid output file name.
+        """
+        self.capture_index= capture_index
+        self.model = self.load_model(model_name)
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("\n\nDevice Used:",self.device)
+
+
+
+    def load_model(self,model_name):
+        """
+        Loads Yolo5 model from pytorch hub.
+        :return: Trained Pytorch model.
+        """
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        return model
+
+
+    def score_frame(self, frame):
+        """
+        Takes a single frame as input, and scores the frame using yolo5 model.
+        :param frame: input frame in numpy/list/tuple format.
+        :return: Labels and Coordinates of objects detected by model in the frame.
+        """
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+     
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
+
+
+    def class_to_label(self, x):
+        """
+        For a given label value, return corresponding string label.
+        :param x: numeric label
+        :return: corresponding string label
+        """
+        return self.classes[int(x)]
+
+
+
 
     def class_to_label(self, x):
         return self.classes[int(x)]
@@ -26,8 +74,8 @@ sys.setrecursionlimit(10 ** 9)
                 bgr = (0, 255, 0)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
                 cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
-
         return frame
+
     def __call__(self):
         # Initialize the  drag object# 
         wName = "Live"
@@ -76,7 +124,6 @@ sys.setrecursionlimit(10 ** 9)
             if cv2.waitKey(1) == ord('q'):
                 break
 
-    
 detector = detection(capture_index=0,model_name='best.pt')
 detector()
 cv2.destroyAllWindows()
